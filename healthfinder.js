e793a669af2cff47aef8b4f533bb8dcc5fc8b8b1
Shalini Kuppa <kuppa.shalini@gmail.com>
2014-08-03 18:21:03 -0400

    Added more fields

--- healthfinder.js
+++ healthfinder.js
@@ -1,3 +1,4 @@
+
 $(function(){
 
     $('#health-finder-form').on('submit', function(e){
@@ -23,7 +24,7 @@ $(function(){
                 var result = responseText.Result;
 
                 var error = result.Error;
-                //debugger;
+               // debugger;
                 if (error === 'False'){
                     var heading = result.MyHFHeading;
                     var total   = result.Total;
@@ -31,11 +32,34 @@ $(function(){
                     var tools = result.Tools;
 
                     var header = $('<div />').addClass('header').html(heading);
-                    var topic_wrap = $('<ul />')
+                    var topic_wrap = $('<ul />');
                     $.each(topics, function(index, topic) {
                         console.log(index, topic);
-                        var li = $('<li />').attr('id', topic.Id).text(topic.Title);
+                        var li = $('<li />').attr('id', topic.Id)
+                        var h2 = $('<h2 />').text(topic.Title);
+                        var img = $('<img />').attr('src', topic.ImageUrl   ).attr('alt', topic.ImageAlt);
+                        var label = $('<label />').addClass('categories').text(topic.Categories);
+                        var a_div = $('<div />');
+                        var  a  = $('<a />').attr('href', topic.AccessibleVersion).text('Accessible Version');
+                        a_div.append(a);
+
+                         var img_logo = $('<img />').attr('src', topic.HealthfinderLogo  ).attr('alt', topic.HealthfinderLogo);
+                          var  a_healthfinder  = $('<a />').attr('href', topic.HealthfinderUrl);
+                            a_healthfinder.append(img_logo);
+                      var Last_Update=$('<time />').text(topic.LastUpdate);
+                        li.append(h2);
+                        li.append(img);
+                        li.append(label);
+                        li.append(a_div);
+                      //  li.append(img_logo);
+                        li.append(a_healthfinder);
+                        li.append(Last_Update);
+
+
+
                         topic_wrap.append(li);
+
+
                     });
 
 
