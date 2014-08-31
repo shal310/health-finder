5d8049c1ab6bf723114cdd2cd81bceb5540ec9db
Shalini Kuppa <kuppa.shalini@gmail.com>
2014-08-31 18:21:37 -0400

    Required settings for ember

---
+++ assets/js/healthfinder.ember.js
@@ -0,0 +1,85 @@
+App = Ember.Application.create({});
+
+App.Router.map(function() {
+  this.resource('posts', function() {
+    this.resource('post', { path: ':topic_id' });
+  });
+  this.resource('topics');
+});
+
+var posts = [{
+  id: '1',
+  title: "Rails is Omakase",
+  author: { name: "d2h" },
+  date: new Date('12-27-2012'),
+  excerpt: "There are lots of à la carte software environments in this world. Places where in order to eat, you must first carefully look over the menu of options to order exactly what you want.",
+  body: "I want this for my ORM, I want that for my template language, and let's finish it off with this routing library. Of course, you're going to have to know what you want, and you'll rarely have your horizon expanded if you always order the same thing, but there it is. It's a very popular way of consuming software.\n\nRails is not that. Rails is omakase."
+}, {
+  id: '2',
+  title: "The Parley Letter",
+  author: { name: "d2h" },
+  date: new Date('12-24-2012'),
+  excerpt: "My [appearance on the Ruby Rogues podcast](http://rubyrogues.com/056-rr-david-heinemeier-hansson/) recently came up for discussion again on the private Parley mailing list.",
+  body: "A long list of topics were raised and I took a time to ramble at large about all of them at once. Apologies for not taking the time to be more succinct, but at least each topic has a header so you can skip stuff you don't care about.\n\n### Maintainability\n\nIt's simply not true to say that I don't care about maintainability. I still work on the oldest Rails app in the world."  
+}];
+
+var posts = [{
+  id: '1',
+  title: "Rails is Omakase",
+  author: { name: "d2h" },
+  date: new Date('12-27-2012'),
+  excerpt: "There are lots of à la carte software environments in this world. Places where in order to eat, you must first carefully look over the menu of options to order exactly what you want.",
+  body: "I want this for my ORM, I want that for my template language, and let's finish it off with this routing library. Of course, you're going to have to know what you want, and you'll rarely have your horizon expanded if you always order the same thing, but there it is. It's a very popular way of consuming software.\n\nRails is not that. Rails is omakase."
+}, {
+  id: '2',
+  title: "The Parley Letter",
+  author: { name: "d2h" },
+  date: new Date('12-24-2012'),
+  excerpt: "My [appearance on the Ruby Rogues podcast](http://rubyrogues.com/056-rr-david-heinemeier-hansson/) recently came up for discussion again on the private Parley mailing list.",
+  body: "A long list of topics were raised and I took a time to ramble at large about all of them at once. Apologies for not taking the time to be more succinct, but at least each topic has a header so you can skip stuff you don't care about.\n\n### Maintainability\n\nIt's simply not true to say that I don't care about maintainability. I still work on the oldest Rails app in the world."  
+}];
+
+App.PostsRoute = Ember.Route.extend({
+  model: function() {
+  	/*
+
+    var api_key = 'omwwjzbtdcuhlnqu'; //registered against my email
+    var age = '25';
+    var gender = 'male';
+    var who = 'someone';
+    var is_pregnant = 0;
+
+    var url = 'http://healthfinder.gov/developer/MyHFSearch.json?api_key= ' + api_key + '&who=' + who + '&age=' + age + '&gender=' + gender + '&pregnant=' + is_pregnant;
+    return $.getJSON(url, {format: "jsonp"}).success(function(data){
+        var result = data.Result;
+        return result.Topics.map(function(topic){
+           topic.body = topic.content;
+           return topic;
+        })
+    });
+    */
+
+    return posts;
+  }
+});
+
+App.PostRoute = Ember.Route.extend({
+  model: function(params) {
+    return posts.findBy('id', params.id);
+  }
+});
+
+App.PostController = Ember.ObjectController.extend({
+  isEditing: false,
+  
+  actions: {
+    edit: function() {
+      this.set('isEditing', true);
+    },
+
+    doneEditing: function() {
+      this.set('isEditing', false);
+    }
+  }
+});
+
